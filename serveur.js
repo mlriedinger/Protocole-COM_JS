e37b78fe10245c8d54dfef5e8fdf18fea9a55497
mlriedinger <marie-laure.riedinger@le-campus-numerique.fr>
2020-05-12 09:31:39 +0200

    Ajout fonctions pour modifier une zone de texte + commentaires

--- serveur.js
+++ serveur.js
@@ -3,6 +3,7 @@ var http = require("http").Server(app);
 var io = require("socket.io")(http);
 
 var counter = 0; // initialise le compteur à 0
+var text = "";
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
@@ -11,6 +12,8 @@ app.get("/", function (req, res) {
 http.listen(3000, function () {
   console.log("listening on *:3000");
 });
+
+// Script pour incrémenter/décrémenter
 
 io.on('connect', function(socket){ // écoute une connexion web socket
     socket.on('increment', function(){ // écoute l'événement 'increment'
@@ -27,3 +30,21 @@ io.on('connect', function(socket){ // écoute une connexion web socket
         io.emit('count', counter); // émet un événement 'count'
     });
 });
+
+// Script pour modifier la zone de texte
+
+io.on('connect', function(socket) {
+    socket.on('modifyText', function(text){
+      console.log(text);
+      io.emit('modifiedText', text);  
+  });
+});
+
+io.on('connect', function(socket) {
+  socket.on('modifyBold', function(text){
+    console.log(text);
+    io.emit('modifiedBold', text);  
+});
+});
+
+
